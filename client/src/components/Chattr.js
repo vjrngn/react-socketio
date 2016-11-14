568bb87cc34c00791babc46186c429eb1964764b
Vijay Rangan <vijay.rangan@gmail.com>
2016-11-14 16:10:10 +0530

    add offline event listener

--- client/src/components/Chattr.js
+++ client/src/components/Chattr.js
@@ -9,18 +9,23 @@ const Chattr = React.createClass({
   displayName: 'Chattr',
 
   componentDidMount() {
-    ChatStore.init()
-    ChatStore.subscribe(Events.INITIALISED, this._updateChatList)
+    ChatStore.init();
+    ChatStore.subscribe(Events.INITIALISED, this._updateChatList);
+    window.addEventListener('online', this._updateNetworkStatus);
+    window.addEventListener('offline', this._updateNetworkStatus);
   },
 
   componentWillUnmount() {
     ChatStore.unsubscribe(Events.INITIALISED, this._updateChatList);
+    window.removeEventListener('online', this._updateNetworkStatus);
+    window.removeEventListener('offline', this._updateNetworkStatus);
   },
 
   getInitialState () {
     return {
       chats: [],
-      currentConversation: null
+      currentConversation: null,
+      offline: false
     }
   },
 
@@ -28,11 +33,27 @@ const Chattr = React.createClass({
     this.setState({ chats: ChatStore.getChatList() })
   },
 
+  _updateNetworkStatus (e) {
+    if (e.type == 'offline') {
+      this.setState({ offline: true })      
+    } else if (e.type == 'online') {
+      this.setState({ offline: false })
+    }
+  },
+
   showSelectedChat (chat) {
     this.setState({ currentConversation: chat });
   },
 
   render () {
+    let networkStatus = '';
+    if (this.state.offline) {
+      networkStatus = (
+        <div className="alert alert-info" style={{ position: 'absolute', bottom: '15px', right: '20px', zIndex: 1000 }}>
+          <strong>Uh oh!</strong> You seem to be offline. You will not be able to send or receive messages while offline.
+        </div>
+      )
+    }
     let conversationPane = '';
     if (this.state.currentConversation) {
       conversationPane = <Conversation chat={this.state.currentConversation} />
@@ -42,6 +63,7 @@ const Chattr = React.createClass({
 
     return (
       <div className="row">
+        {networkStatus}
         <div className="col-sm-4">
           <ChatList chats={this.state.chats} onChatSelect={this.showSelectedChat} />
         </div>
