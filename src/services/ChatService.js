f8d14e303d8a34e3a83b7b486416302c4fe0643f
Vijay Rangan <vijay.rangan@gmail.com>
2016-11-14 15:09:09 +0530

    dir cleanup

--- src/services/ChatService.js
+++
@@ -1,9 +0,0 @@
-export default {
-  getAllChats () {
-    return fetch('/chatList').then(response => {
-      return response.json()
-    }).then(chats => {
-      return chats;
-    }).catch(err => console.log(err));
-  }
-}
